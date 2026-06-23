7df67c1960e1f71e130aa7d3793a31184423f2d7
ultimatedev03 <ultimatedev2025@gmail.com>
2026-06-23 13:28:26 +0530

    done by dp

--- server/routes/publicConfig.js
+++ server/routes/publicConfig.js
@@ -5,6 +5,14 @@ const router = express.Router();
 
 const MAINTENANCE_KEY = 'maintenance_mode';
 
+router.use((_req, res, next) => {
+  res.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.set('Pragma', 'no-cache');
+  res.set('Expires', '0');
+  res.set('Surrogate-Control', 'no-store');
+  next();
+});
+
 const defaultPublicConfig = {
   maintenance_mode: false,
   maintenance_message: '',
